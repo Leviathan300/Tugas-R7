337f7b2b67b320556e6f264a9db56b573da9693d
Leviathan300 <104558368+Leviathan300@users.noreply.github.com>
2024-01-11 21:39:28 +0700

    Add files via upload

---
+++ TugasBus/testBusPratikum.java
@@ -0,0 +1,15 @@
+package produck.inventory;
+
+public class testBusPratikum {
+    public static void main(String[] args) {
+        Main busMini = new Main(10);
+        busMini.addPenumpang(5);
+        busMini.addPenumpang(6);
+        busMini.getPenumpang(2);
+        busMini.getPenumpang(1);
+        busMini.cetak();
+        
+        busMini.getAverage();
+        busMini.cetak();
+    }
+}
