11b34a064d5f6e09f9e400fb88b6fa9ddc69b98d
Leviathan300 <104558368+Leviathan300@users.noreply.github.com>
2024-01-11 21:39:11 +0700

    Add files via upload

---
+++ TugasBus/Bus1.java
@@ -0,0 +1,10 @@
+package produck.inventory;
+
+public class Bus {
+public int penumpang,maxPenumpang;
+
+public void cetak(){
+    System.out.println("Penumpang sekarang : " + penumpang);
+    System.out.println("Penumpang seharusnya adalah : " + maxPenumpang);
+}
+}
